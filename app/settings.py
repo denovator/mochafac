8158d4070232e4a3485bc4b6a90820fbe4b1ef98
Seungho <seunghojung0114@gmail.com>
2014-08-05 22:39:10 +0900

    first commit

---
+++ app/settings.py
@@ -0,0 +1,10 @@
+class Config(object):
+    SECRET_KEY = "asdfjlsadjflkjlwkqjerljlasdjfl"
+    debug = False
+
+
+class Production(Config):
+    debug = True
+    CSRF_ENABLED = False
+    ADMIN = "seunghojung0114@gmail.com"
+    SQLALCHEMY_DATABASE_URI = "mysql+gaerdbms:///flaskr?instance=mochafac:flaskr-instance"
