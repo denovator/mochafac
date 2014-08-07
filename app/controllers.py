620a94953b9d5d38cb195f2c7ae9ddb3cc7f4df9
Seungho <seunghojung0114@gmail.com>
2014-08-07 22:55:18 +0900

    ---

--- app/controllers.py
+++ app/controllers.py
@@ -38,3 +38,38 @@ def article_create():
             flash(u'게시글을 작성하였습니다.', 'success')
             return redirect(url_for('article_list'))
         return render_template('article/create.html', form=form, active_tab='article_create')
+
+
+@app.route('/article/detail/<int:id>', methods=['GET'])
+def article_detail(id):
+    article = Article.query.get(id)
+
+    # relationship을 활용한 query
+    comments = article.comments.order_by(desc(Comment.date_created)).all()
+
+    return render_template('article/detail.html', article=article, comments=comments)
+
+
+@app.route('/comment/create/<int:article_id>', methods=['GET', 'POST'])
+def comment_create(article_id):
+    form = CommentForm()
+    if request.method == 'GET':
+        return render_template('comment/create.html', form=form)
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            # 사용자가 입력한 글 데이터로 Comment 모델 인스턴스를 생성한다.
+            comment = Comment(
+                author=form.author.data,
+                email=form.email.data,
+                content=form.content.data,
+                password=form.password.data,
+                article=Article.query.get(article_id)
+            )
+            # 데이터베이스에 데이터를 저장할 준비를 한다. (댓글)
+            db.session.add(comment)
+            # 데이터베이스에 저장하라는 명령을 한다.
+            db.session.commit()
+
+            flash(u'댓글을 작성하였습니다.', 'success')
+            return redirect(url_for('article_detail', id=article_id))
+        return render_template('comment/create.html', form=form, active_tab='article_create')
